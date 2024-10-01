5b066aa857eeadf0a7ab362d6ac07536c57b9478
Thomas Hodges <thodges@javascriptonline.org>
2024-09-30 21:48:18 -0700

    date and reverse order

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,20 +1,25 @@
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postsSlice';
 import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
+  const renderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+    .map((post) => (
+      <article key={post.id}>
+        <h3>{post.title}</h3>
 
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-      </p>
-    </article>
-  ));
+        <p>{post.content.substring(0, 100)}</p>
+        <p className="postCredit">
+          <PostAuthor userId={post.userId} />
+          <TimeAgo timestamp={post.date} />
+        </p>
+      </article>
+    ));
 
   return (
     <section>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,19 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = [
   {
     id: '1',
     title: 'Learn Redux Toolkit',
     content: 'Redux Toolkit is a modern alternative to Redux.',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: '2',
     title: 'Use Redux Toolkit with React',
     content:
       'Redux Toolkit makes it easy to write Redux logic in a type-safe manner.',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ];
 
@@ -27,6 +30,7 @@ const postsSlice = createSlice({
           id: nanoid(),
           title,
           content,
+          date: new Date().toISOString(),
           userId,
         },
       }),
